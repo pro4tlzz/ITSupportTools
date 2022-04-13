05e047476471b6fea8de328671e4e23adf1e8b43
Pro4TLZZ <github@pro4tlzz.com>
2022-04-13 09:44:23 +0100

    use dict for row and add  lastVerified

--- okta/mfa/list-users-factors.py
+++ okta/mfa/list-users-factors.py
@@ -12,15 +12,24 @@ headers = {
 session = requests.Session()
 session.headers.update(headers)
 
-base_url="https://$org.okta.com"
-group="$group"
+base_url="https://.okta.com"
+group=""
 url=f"{base_url}/api/v1/groups/{group}/users"
 
-list_of_rows=["user_id","user_status","username","factor_id","factor_type","factor_created","factor_lastUpdated"]
+data={
+    "user_id": None,
+    "user_status": None,
+    "username": None,
+    "factor_id": None,
+    "factor_type": None,
+    "factor_created": None,
+    "factor_lastUpdated": None,
+    "factor_lastVerified": None
+}
 
-def list_users(url):
+def list_users(url,data):
 
-    make_csv(list_of_rows)
+    make_csv(data)
 
     while url:
 
@@ -33,11 +42,11 @@ def list_users(url):
             user_id=user["id"]
             user_status=user["status"]
             username=user["profile"]["login"]
-            data={
-                "user_id":  user_id,
-                "user_status":  user_status,
-                "username":  username
-            }
+
+            data["user_id"]=user_id
+            data["user_status"]=user_status
+            data["username"]=username
+
             print(data)
             if user_status == "ACTIVE":
 
@@ -46,7 +55,7 @@ def list_users(url):
         next = response.links.get('next')
         url = next['url'] if next else None
 
-def get_factors(user_id,user_data):
+def get_factors(user_id,data):
 
     url=f"{base_url}/api/v1/users/{user_id}/factors"
     response = session.get(url)
@@ -60,14 +69,20 @@ def get_factors(user_id,user_data):
         factor_created=factor["created"]
         factor_lastUpdated=factor["lastUpdated"]
 
-        user_data["factor_id"]=factor_id
-        user_data["factor_type"]=factor_type
-        user_data["factor_created"]=factor_created
-        user_data["factor_lastUpdated"]=factor_lastUpdated
+        try:
+            factor_lastVerified=factor["lastVerified"]
+        except:
+            factor_lastVerified=""
 
-        update_csv(user_data)
+        data["factor_id"]=factor_id
+        data["factor_type"]=factor_type
+        data["factor_created"]=factor_created
+        data["factor_lastUpdated"]=factor_lastUpdated
+        data["factor_lastVerified"]=factor_lastVerified
 
-        print(user_data)
+        update_csv(data)
+
+        print(data)
 
 def make_csv(rows):
 
@@ -82,4 +97,4 @@ def update_csv(data):
         w = csv.writer(f)
         w.writerow(data.values())
 
-list_users(url)
+list_users(url,data)

c32173421729c2d5ee3d478e60ca1e4550718290
Pro4TLZZ <github@pro4tlzz.com>
2022-05-31 13:09:53 +0100

    reset pw and notify email

---
+++ okta/user/reset-password-notify-email.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+import requests
+import os
+import smtplib, ssl
+from csv import DictReader
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+api_key=os.environ['okta_api_token']
+gmail_smtp_username=os.environ['gmail_smtp_username']
+gmail_smtp_password=os.environ['gmail_smtp_password']
+
+
+headers = {
+    'Authorization': 'SSWS ' + api_key,
+    'Accept': 'application/json'
+}
+
+session = requests.Session()
+session.headers.update(headers)
+
+base_url="https://domain.okta.com"
+filename="users.csv"
+
+
+def reset_password(username):
+
+    url=f"{base_url}/api/v1/users/{username}/lifecycle/expire_password?tempPassword=true"
+    response=session.post(url)
+    response.raise_for_status
+
+    api_response=response.json()
+    print(api_response)
+    temp_password=api_response['tempPassword']
+
+    return temp_password
+
+def send_email(username,temp_password):
+
+    context = ssl.create_default_context()
+    oubtbound_host = 'smtp.gmail.com'
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    message["From"] = gmail_smtp_username
+    message["To"] = username
+
+    # Create the plain-text and HTML version of your message
+    text = f"""\
+    Hi {username},
+
+    Your Okta password has been reset to {temp_password}
+    Please change it at {base_url}
+
+    Thanks
+    """
+    html = f"""\
+    <html>
+    <body>
+        <p>Hi {username},<br>
+        <br>
+        Your Okta password has been reset to {temp_password}<br>
+        Please change it at {base_url}<br>
+        <br>
+        Thanks<br>
+        </p>
+    </body>
+    </html>
+    """
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    with smtplib.SMTP(oubtbound_host, 587) as server:
+        server.debuglevel=1
+        server.ehlo()
+        server.connect(oubtbound_host, 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_smtp_username, gmail_smtp_password)
+        server.sendmail(gmail_smtp_username, username, message.as_string())
+    
+    server.close
+
+def read_csv():
+
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        reader=DictReader(f)
+        for row in reader:
+            username=row["username"]
+            temp_password=reset_password(username)
+            send_email(username,temp_password)
+
+
+if __name__ == '__main__':
+
+    read_csv()

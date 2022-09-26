a8addef7bf00251ed820b09ff175c2a64caf7822
Pro4TLZZ <github@pro4tlzz.com>
2022-09-26 21:49:33 +0100

    update script

--- javascript/get_zone_cloudflare().js
+++ javascript/get_zone_cloudflare().js
@@ -1,6 +1,6 @@
 async function get_zone() {
 	const domain = window.prompt("domain please","");
-    url = "https://api.cloudflare.com/client/v4/zones?name=" + domain
+    url = "https://api.cloudflare.com/client/v4/zones?name=" + domain;
 	const token = window.prompt("token please","");
 	const email = window.prompt("email please","");
     headers = {
@@ -9,7 +9,11 @@ async function get_zone() {
 		"X-Auth-Email": '' + email,
 		"Content-Type": "application/json"
 	}
-	response = await fetch(url, {headers, method: 'get'})
-	api_response = await response.json()
-	console.log(api_response)
+	response = await fetch(url, {headers, method: 'get'});
+	result = await response.json();
+	zone = result.result[0].id;
+	console.log(zone);
 }
+get_zone()
+
+// because of CORS you need to run from https://api.cloudflare.com/

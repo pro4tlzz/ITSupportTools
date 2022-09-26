307cfbd9b2f2714294ea5690492034beb3d48d66
Pro4TLZZ <github@pro4tlzz.com>
2022-09-26 22:52:24 +0100

    add get request for records

--- javascript/get-zone-records.js
+++ javascript/get-zone-records.js
@@ -1,6 +1,6 @@
 (async function() {
 
-    const headers = {
+    var headers = {
         "Accept": "*/*",
         "sec-fetch-dest": "document" ,
         "sec-fetch-mode": "navigate",
@@ -10,15 +10,34 @@
     }
 
     const atok = await get_Token();
+    const records = await get_Zone_Records(atok)
     console.log(atok)
+    console.log(records)
         
     async function get_Token() {
+        
         const url = "https://dash.cloudflare.com/api/v4/system/bootstrap"
-            
         const r = await fetch(url, {headers, method: 'get', credentials: 'include'});
         const result = await r.json();
         const token = result.result.data.atok;
+        
         return token
+    }
+
+    async function get_Zone_Records(atok) {
+        
+        var pathArray = window.location.pathname.split('/');
+        var zoneId = pathArray[1];
+        
+        console.log(zoneId);
+        const url = "https://dash.cloudflare.com/api/v4/zones/" + zoneId + "/dns_records?per_page=50";
+        console.log(url);
+        headers['x-atok'] = atok;
+        console.log(headers);
+        const r = await fetch(url, {headers, method: 'get', credentials: 'include'});
+        const result = await r.json();
+        
+        return result
     }
 }
 )();

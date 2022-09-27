613dbe5af0e3b463f67c3f7c1f3996ab7faa5009
Gabriel Sroka <gabrielsroka@gmail.com>
2022-09-27 14:41:26 -0700

    Update getCloudflareZoneRecords.js
    what url has the `pre` in it?

--- javascript/getCloudflareZoneRecords.js
+++ javascript/getCloudflareZoneRecords.js
@@ -1,55 +1,37 @@
-(async function() {
-
-    const zoneId = await getZoneId();
-    
-    if (zoneId != false) {
+(async function () {
+    const zoneId = getZoneId();
+    if (zoneId) {
         const atok = await getToken();    
-        const records = await getZoneRecords(atok,zoneId);
+        const records = await getZoneRecords(atok, zoneId);
         console.table(records);
+    } else {
+        console.log('No zone id provided');
     }
 
     function getZoneId() {
-
-        const zoneId = document.querySelector('pre')?.innerHTML;
-
-        if (zoneId) { return zoneId; }
-            
-        else {
-            const zoneId = prompt("Please enter zone id:");
-            if (zoneId) {return zoneId;}
-            else {
-                alert("No value provided");
-                return false;
-                 }
+        var zoneId = document.querySelector('pre')?.innerHTML;
+        if (!zoneId) {
+            zoneId = prompt('Please enter zone id:');
         }
-        
+        return zoneId;
     }    
         
     async function getToken() {
-        
-        const url = "https://dash.cloudflare.com/api/v4/system/bootstrap";
-        const headers = {
-            'x-cross-site-security': 'dash'
-        }
-        headers['x-cross-site-security'] = 'dash';
-        const r = await fetch(url, {headers, method: 'get'});
-        const result = await r.json();
-        const token = result.result.data.atok;
+        const url = '/api/v4/system/bootstrap';
+        const r = await fetch(url);
+        const res = await r.json();
+        const token = res.result.data.atok;
         return token;
-        
     }
 
-    async function getZoneRecords(atok,zoneId) {
-
-        const url = "/api/v4/zones/" + zoneId + "/dns_records?per_page=50";
+    async function getZoneRecords(atok, zoneId) {
+        const url = '/api/v4/zones/' + zoneId + '/dns_records?per_page=50';
         const headers = {
             'x-atok': atok
-        }
-        const r = await fetch(url, {headers, method: 'get'});
-        const result = await r.json();
-        const records = result.result;
+        };
+        const r = await fetch(url, {headers});
+        const res = await r.json();
+        const records = res.result;
         return records;
-        
     }
-}
-)();
+})();

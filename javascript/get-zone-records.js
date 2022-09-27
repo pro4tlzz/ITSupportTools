6f9f909c431970765d71c58f4047553244e40bdb
Pro4TLZZ <github@pro4tlzz.com>
2022-09-27 21:02:25 +0100

    clean code

--- javascript/get-zone-records.js
+++ javascript/get-zone-records.js
@@ -1,42 +1,37 @@
 (async function() {
+
+    const zoneId = await getZoneId();
     
-    var headers = {
-        
-        "Accept": "application/json",
-        "x-cross-site-security": "dash"    
-        
+    if (zoneId != false) {
+        const atok = await getToken();    
+        const records = await getZoneRecords(atok,zoneId);
+        console.table(records);
     }
 
-    zoneId = await getZoneId();
-    const atok = await getToken();    
-    const records = await getZoneRecords(atok,zoneId);
-    console.table(records);
+    function getZoneId() {
 
-    async function getZoneId() {
-        
-        try {
-        
-            const zoneId = document.querySelector('pre').innerHTML;
-            return zoneId;
-        
-        }
-        
-        catch(err) {
-        
-            const zoneId = prompt("Please enter zone id:");
-            if (zoneId == "") {
-                alert("No value provided");
-                return;
-            }
+        const zoneId = document.querySelector('pre')?.innerHTML;
+
+        if (zoneId) { return zoneId; }
             
-            return zoneId;
-        
+        else {
+            const zoneId = prompt("Please enter zone id:");
+            if (zoneId) {return zoneId;}
+            else {
+                alert("No value provided");
+                return false;
+                 }
         }
+        
     }    
         
     async function getToken() {
         
         const url = "https://dash.cloudflare.com/api/v4/system/bootstrap";
+        const headers = {
+            'x-cross-site-security': 'dash'
+        }
+        headers['x-cross-site-security'] = 'dash';
         const r = await fetch(url, {headers, method: 'get'});
         const result = await r.json();
         const token = result.result.data.atok;
@@ -46,8 +41,10 @@
 
     async function getZoneRecords(atok,zoneId) {
 
-        const url = "https://dash.cloudflare.com/api/v4/zones/" + zoneId + "/dns_records?per_page=50";
-        headers['x-atok'] = atok;
+        const url = "/api/v4/zones/" + zoneId + "/dns_records?per_page=50";
+        const headers = {
+            'x-atok': atok
+        }
         const r = await fetch(url, {headers, method: 'get'});
         const result = await r.json();
         const records = result.result;

b2ab46f3870301660554462c79158130e2aeaee3
Pro4TLZZ <github@pro4tlzz.com>
2022-09-27 08:34:42 +0100

    update headers and auth

--- javascript/get-zone-records.js
+++ javascript/get-zone-records.js
@@ -1,36 +1,30 @@
 (async function() {
 
     const zoneId = document.querySelector('pre').innerHTML;
-    
+
     var headers = {
         "Accept": "*/*",
-        "sec-fetch-dest": "document" ,
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-gpc": "1",
         "x-cross-site-security": "dash"    
     }
 
-    const atok = await get_Token();    
-    const records = await get_Zone_Records(atok,zoneId);
-    console.log(records);
+    const atok = await getToken();    
+    const records = await getZoneRecords(atok,zoneId);
+    console.table(records);
         
-    async function get_Token() {
+    async function getToken() {
         
         const url = "https://dash.cloudflare.com/api/v4/system/bootstrap"
-        const r = await fetch(url, {headers, method: 'get', credentials: 'include'});
+        const r = await fetch(url, {headers, method: 'get'});
         const result = await r.json();
         const token = result.result.data.atok;
         return token
     }
 
-    async function get_Zone_Records(atok,zoneId) {
+    async function getZoneRecords(atok,zoneId) {
 
         const url = "https://dash.cloudflare.com/api/v4/zones/" + zoneId + "/dns_records?per_page=50";
         headers['x-atok'] = atok;
-        headers['sec-fetch-dest'] = 'empty';
-        headers['sec-fetch-mode'] = 'cors';
-        const r = await fetch(url, {headers, method: 'get', credentials: 'include'});
+        const r = await fetch(url, {headers, method: 'get'});
         const result = await r.json();
         const records = result.result;
         return records

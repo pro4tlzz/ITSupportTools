58170820b5f0c9ff5199a9b1ad469bc0ace420b1
Pro4TLZZ <github@pro4tlzz.com>
2022-09-30 12:28:10 +0100

    WIP
    prompt user for group rule id and evaluate all users of group against that rule

---
+++ javascript/evalGroupRuleNew.js
@@ -0,0 +1,85 @@
+(async function() {
+
+    const headers = {
+        'accept': 'application/json',
+        'content-type' : 'application/json',
+        'X-Okta-XsrfToken': document.querySelector('#_xsrfToken').innerText
+    }
+    
+    const groupId = getGroupId();
+    const groupRuleId = getGroupRuleId();
+
+    if (groupRuleId != false) {
+
+        const groupMembers = await getGroupMembers(groupId);
+        const rule = await getGroupRule(groupRuleId);
+        const tableResult = [];
+
+        for (const user of groupMembers) {
+            const userId = user.id ;
+            const eval = await evalGroupRule(rule,userId,tableResult);
+        }
+
+        console.table(tableResult);
+    }
+
+    // from Rockstar https://github.com/gabrielsroka/gabrielsroka.github.io/blob/master/rockstar/rockstar.js#L774
+    function getGroupId() {
+        var path = location.pathname;
+        var pathparts = path.split('/');
+        if (path.match("admin/group") && (pathparts.length == 4)) {
+            return pathparts[3];
+        }
+    }
+
+    function getGroupRuleId() {
+        
+        const groupRuleId = prompt('Please enter the group rule id');
+        if (groupRuleId) {
+            return groupRuleId;
+        }
+        else {
+            alert("No value provided");
+            return false;
+        }
+    }
+
+    async function getGroupRule(groupRuleId) {
+
+        const url = '/api/v1/groups/rules/' + groupRuleId;
+        const r = await fetch(url, {headers});
+        const rule = await r.json();
+        return rule;
+
+    }
+
+    async function getGroupMembers(groupId) {
+
+        const url = '/api/v1/groups/' + groupId + '/users';
+        const r = await fetch(url, {headers});
+        const groupMembers = await r.json();
+        return groupMembers;
+
+    }
+
+    async function evalGroupRule(rule,userId,tableResult) {
+        
+        const ruleValue = rule.conditions.expression.value;
+        const url = '/api/v1/internal/expression/eval';        
+        const body = JSON.stringify([{
+            "type":"urn:okta:expression:1.0",
+            "value": ruleValue,
+            "targets":{"user":userId},
+            "operation":"CONDITION"
+        }]);
+        console.log(body);
+        const r = await fetch(url, {method: 'post', body, headers});
+        const eval = await r.json();
+        const result = eval[0].result;
+        console.log(result);
+        tableResult.push(ruleValue,userId,result)
+        return result;
+    }
+
+}
+)();

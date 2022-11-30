9db56efbfebe8c168d0af9e7fb2883bff8a5c429
Gabriel Sroka <gabrielsroka@gmail.com>
2022-11-30 15:30:07 -0800

    Update convert-individual-assignment-to-group.py

--- okta/apps/convert-individual-assignment-to-group.py
+++ okta/apps/convert-individual-assignment-to-group.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
+
 import requests
 import os
 import time
 
-okta_api_token_test=os.environ['okta_api_token_test']
+# Set these:
+okta_api_token = os.environ['okta_api_token']
+base_url = "https://-test-admin.okta.com"
+appids = ['$appidlist']
 
 headers = {
-    'Authorization': 'SSWS ' + okta_api_token_test,
+    'Authorization': 'SSWS ' + okta_api_token,
     'Accept': 'application/json'
 }
-
 session = requests.Session()
 session.headers.update(headers)
 
-base_url="https://-test-admin.okta.com"
-apps=['$appidlist']
+def convert_assignments(appid):
 
-def convert_assignments(app):
+    url = f"{base_url}/api/v1/appAssignments/instance/{appid}/async/convertAll"
+    response = session.post(url)
+    response.raise_for_status()
 
-    url=f"{base_url}/api/v1/appAssignments/instance/{app}/async/convertAll"
-    response=session.post(url)
-    response.raise_for_status
-
-    convert_result=response.json()
+    convert_result = response.json()
     print(convert_result)
 
-    timer=2
+    timer = 2
+    status = None
+    while status != 'COMPLETED':
 
-    while url:
+        response = session.get(url)
+        response.raise_for_status()
 
-        response=session.get(url)
-        response.raise_for_status
-
-        convert_result=response.json()
-        status=convert_result['status']
+        convert_result = response.json()
+        status = convert_result['status']
         print(convert_result)
         print(f"Going to sleep for {timer}")
         time.sleep(timer)
-        timer=timer*2
-        if status == 'COMPLETED':
-            url = None
+        timer = timer*2
 
 if __name__ == '__main__':
 
-    for app in apps:
-        convert_assignments(app)
+    for appid in appids:
+        convert_assignments(appid)
